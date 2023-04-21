365d0011dbfaf9f8d54cbba7d90db59e4a6f02d7
Ubuntu <meet@ubuntu>
2023-04-22 04:29:19 +0530

    added comments.sh file.

---
+++ comments.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+# Examples of different type of comments on bash scripts
+
+# This is a single line comment and it is starting from the beginning of the line.
+
+echo "This is an in-line comment and is used to describe something like a variable." # Some explanation about this line.
+
+: 'This is an example,
+of a multi-line comment
+that can used to give 
+information abour a
+function or about the
+program as well'
+
